f458643cfcbac45a4b2d2e28b0e0597976aeee17
nafizadaNC <Nurcan.Nafizada@kapitalbank.az>
2026-04-03 09:49:01 +0400

    initial

---
+++ src/main/java/com/example/digitalwallet/domain/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.digitalwallet.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String role;
+
+}
